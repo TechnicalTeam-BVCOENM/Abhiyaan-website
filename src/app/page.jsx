6ddbf7a6fbb597f88697a4219b2d54396a67115f
Rushikesh <somvanshihrishikesh7@gmail.com>
2024-01-13 20:31:31 +0530

    feat: Map & footer added

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,13 @@
 'use client';
+import Map from '@/components/map/map';
+import Footer from '@/components/footer/footer';
 // import Spline from '@splinetool/react-spline';
 
 export default function Home() {
-  return <center className='mx-12 mt-16 scale-105 overflow-x-hidden'></center>;
+  return (
+    <center className='mx-12 mt-16 scale-105 overflow-hidden'>
+      <Map />
+      <Footer />
+    </center>
+  );
 }
---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,97 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faInstagram,
+  faFacebook,
+  faYoutube,
+  faTwitter,
+  faLinkedin,
+  faTelegram,
+} from '@fortawesome/free-brands-svg-icons';
+
+const Footer = () => {
+  const Icons = [
+    {
+      icon: faInstagram,
+      class:
+        'transition duration-400 ease-in-out  fa-brands fa-instagram text-3xl md:text-5xl hover:scale-110 ',
+      id: 1,
+      link: 'https://www.instagram.com/bvcoenm/',
+    },
+    {
+      icon: faFacebook,
+      class:
+        'transition duration-400 ease-in-out  fa-brands fa-square-facebook text-3xl md:text-5xl  hover:scale-110',
+      id: 2,
+      link: 'https://www.facebook.com/bvcoenmedu/',
+    },
+    {
+      icon: faYoutube,
+      class:
+        'transition duration-400 ease-in-out  fa-brands fa-youtube text-3xl md:text-5xl  hover:scale-110',
+      id: 3,
+      link: 'https://www.youtube.com/channel/UC4Pwf6mDh3k0ra0p7QWDPnQ',
+    },
+    {
+      icon: faTwitter,
+      class:
+        'transition duration-400 ease-in-out  fa-brands fa-square-twitter text-3xl md:text-5xl  hover:scale-110',
+      id: 4,
+      link: 'https://twitter.com/abhiyaan_bvcoe',
+    },
+    {
+      icon: faLinkedin,
+      class:
+        'transition duration-400 ease-in-out  fa-brands fa-linkedin text-3xl md:text-5xl  hover:scale-110',
+      id: 5,
+      link: 'https://www.linkedin.com/school/bharati-vidyapeeth-college-of-engineering-navi-mumbai/',
+    },
+    {
+      icon: faTelegram,
+      class:
+        'transition duration-400 ease-in-out  fa-brands fa-telegram text-3xl md:text-5xl  hover:scale-110',
+      id: 6,
+      link: 'https://t.me/bvcoenm',
+    },
+  ];
+  return (
+    <div id='footer' className='z-10 h-auto w-auto bg-black'>
+      <div
+        id='footer_texts'
+        className='social-links flex flex-col items-center justify-between text-left md:flex-row md:items-start'
+      >
+        <div className='mb-4 text-center md:mb-0'>
+          © 2024 BVCOE, Navi Mumbai |
+          <br className='md:hidden' /> All Rights Reserved
+        </div>
+        <div className='social-links'>
+          {Icons.map((icon) => (
+            <a
+              href={icon.link}
+              key={icon.id}
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <span>
+                <FontAwesomeIcon
+                  icon={icon.icon}
+                  id='fa_icon'
+                  style={{ color: '#ffffff', margin: 5 }}
+                  aria-label={`Visit ${icon.link}`}
+                />
+              </span>
+            </a>
+          ))}
+        </div>
+      </div>
+      <div id='footer_text' className='p-4 text-center'>
+        <h1>
+          Designed And Developed By{' '}
+          <span className='font-bold text-orange-500'>Technical Team</span>
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
