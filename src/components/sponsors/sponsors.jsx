4140fa8da1e4058063d0de6d71ceb83c3d68429c
Sambhav Rana <sambhavrana27@gmail.com>
2024-01-14 18:49:07 +0530

    feat: Made Sponsors page responsive

--- src/components/sponsors/sponsors.jsx
+++ src/components/sponsors/sponsors.jsx
@@ -3,38 +3,30 @@ import { sponsers } from './sponsors-model';
 
 const SponsorsPage = () => {
   return (
-    <div
-      id='sponsors'
-      className='z-50 h-auto w-screen select-none bg-black px-4 pt-20'
-    >
-      <div className='font-openSans  mb-4 overflow-hidden text-center text-5xl text-white md:text-6xl'>
-        <h1 className='duration-400 overflow-hidden text-orange-600 transition ease-in-out hover:text-orange-600 lg:text-white'>
-          Our Sponsers
-        </h1>
-      </div>
-      <div className='grid grid-cols-1 items-center justify-items-center gap-2 bg-black p-16 md:grid-cols-5 md:gap-10 md:p-2 lg:gap-10 lg:p-32'>
-        {sponsers.map((sp) => {
-          return (
-            <div
-              key={sp.id}
-              className='duration-400 h-auto w-auto p-8  transition  ease-in-out hover:scale-105 hover:ease-in-out  md:p-0 lg:p-3'
+    <div className='bg-black p-4 text-white'>
+      <h1 className='mb-4 text-center text-5xl text-orange-600 hover:text-orange-600 lg:text-white'>
+        Our Sponsors
+      </h1>
+      <div className='grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-5'>
+        {sponsers.map((sponsor) => (
+          <div key={sponsor.id} className='p-4'>
+            <a
+              href={sponsor.link}
+              target='_blank'
+              rel='noopener noreferrer'
+              className='block'
             >
-              <a href={sp.link} target='_blank'>
-                <img
-                  src={sp.img}
-                  loading='lazy'
-                  className='h-auto w-auto'
-                  alt={sp.name}
-                />
-              </a>
-            </div>
-          );
-        })}
+              <img
+                src={sponsor.img}
+                alt={sponsor.name}
+                className='h-auto w-full'
+              />
+            </a>
+          </div>
+        ))}
       </div>
     </div>
   );
 };
 
 export default SponsorsPage;
-
-// grid m daalhe H AND W
