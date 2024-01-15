52c96eb1dd17701440c642460a98960d102fc0e0
Rushikesh <somvanshihrishikesh7@gmail.com>
2024-01-15 16:48:06 +0530

    feat: Model added

---
+++ src/components/footer/footer-model.js
@@ -0,0 +1,57 @@
+import {
+  faInstagram,
+  faFacebook,
+  faYoutube,
+  faSquareXTwitter,
+  faLinkedin,
+  faTelegram,
+} from '@fortawesome/free-brands-svg-icons';
+
+export const Icons = [
+  {
+    icon: faInstagram,
+    class:
+      'transition duration-400 ease-in-out  fa-brands fa-instagram text-3xl md:text-5xl hover:scale-110 ',
+    id: 1,
+    link: 'https://www.instagram.com/abhiyaan_bvcoenm/',
+  },
+  {
+    icon: faFacebook,
+    class:
+      'transition duration-400 ease-in-out  fa-brands fa-square-facebook text-3xl md:text-5xl  hover:scale-110',
+    id: 2,
+    link: 'https://www.facebook.com/bvcoenmedu/',
+  },
+  {
+    icon: faYoutube,
+    class:
+      'transition duration-400 ease-in-out  fa-brands fa-youtube text-3xl md:text-5xl  hover:scale-110',
+    id: 3,
+    link: 'https://www.youtube.com/channel/UC4Pwf6mDh3k0ra0p7QWDPnQ',
+  },
+  {
+    icon: faSquareXTwitter,
+    class:
+      'transition duration-400 ease-in-out  fa-brands fa-square-twitter text-3xl md:text-5xl  hover:scale-110',
+    id: 4,
+    link: 'https://twitter.com/abhiyaan_bvcoe',
+  },
+  {
+    icon: faLinkedin,
+    class:
+      'transition duration-400 ease-in-out  fa-brands fa-linkedin text-3xl md:text-5xl  hover:scale-110',
+    id: 5,
+    link: 'https://www.linkedin.com/school/bharati-vidyapeeth-college-of-engineering-navi-mumbai/',
+  },
+  {
+    icon: faTelegram,
+    class:
+      'transition duration-400 ease-in-out  fa-brands fa-telegram text-3xl md:text-5xl  hover:scale-110',
+    id: 6,
+    link: 'https://t.me/bvcoenm',
+  },
+];
+
+module.export = {
+  Icons,
+};
