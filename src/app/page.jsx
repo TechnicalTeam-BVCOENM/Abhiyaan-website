85b377e0664a1885e53bef08c9d06ece9869d30e
Santosh Phadtare <santoshphadtare1610@gmail.com>
2024-01-16 21:57:10 +0530

    Issue resolved

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,12 +1,12 @@
 'use client';
-import Content from '@/components/sponsors/sponsors';
+import Sponsors from '@/components/sponsors/sponsors';
 import Map from '@/components/map/map';
 import Footer from '@/components/footer/footer';
 
 export default function Home() {
   return (
     <center className='mx-12 mt-16 scale-105 overflow-x-hidden'>
-      <Content />
+      <Sponsors/>
       <Map />
       <Footer />
     </center>
