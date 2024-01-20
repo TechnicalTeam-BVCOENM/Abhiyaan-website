b0f9cbb3a16a6341ecb978437d20e5d1f349b3f2
psantosh16 <santoshphadtare1610@gmail.com>
2024-01-20 22:54:22 +0530

    feat: Events resposiveness added

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,17 +2,14 @@
 import Sponsors from '@/components/sponsors/sponsors';
 import Map from '@/components/map/map';
 import Footer from '@/components/footer/footer';
-import Promotion from '@/components/promotion/promotion';
 import SectionDivider from '@/components/common/section_divider';
 import Events from '@/components/events/events';
 
 export default function Home() {
   return (
     <div className=''>
-      <Events/>
-      <h1 className='h-screen' >Test</h1>
-      <Promotion />
-
+      <SectionDivider title='Events' />
+      <Events />
       <SectionDivider title='Sponsors' />
       <Sponsors />
       <SectionDivider title='Contact Us' />
