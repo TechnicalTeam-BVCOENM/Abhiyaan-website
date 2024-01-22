9a72b74f4052813a85f0fad7a27c17e695220eeb
rohitbaing08 <rohitbaing456@gmail.com>
2024-01-22 22:09:09 +0530

    fix: Event cards data mapping fixed

---
+++ src/components/events/event_model.js
@@ -0,0 +1,30 @@
+export const events = [
+    {
+      id: 4,
+      imageSrc:
+        'https://media.istockphoto.com/id/1297159365/photo/portrait-of-young-smiling-woman-face-partially-covered-with-flying-hair-in-windy-day-standing.jpg?s=612x612&w=0&k=20&c=I16c_ZzQHEeGoPUVrVP9pPusSzsmymvVKdQVgPuVdDo=',
+      title: 'Fake Event 4',
+      description: 'This is a fake event description 4',
+    },
+    {
+      id: 5,
+      imageSrc:
+        'https://media.istockphoto.com/id/1297159365/photo/portrait-of-young-smiling-woman-face-partially-covered-with-flying-hair-in-windy-day-standing.jpg?s=612x612&w=0&k=20&c=I16c_ZzQHEeGoPUVrVP9pPusSzsmymvVKdQVgPuVdDo=',
+      title: 'Fake Event 5',
+      description: 'This is a fake event description 5',
+    },
+    {
+      id: 6,
+      imageSrc:
+        'https://media.istockphoto.com/id/1297159365/photo/portrait-of-young-smiling-woman-face-partially-covered-with-flying-hair-in-windy-day-standing.jpg?s=612x612&w=0&k=20&c=I16c_ZzQHEeGoPUVrVP9pPusSzsmymvVKdQVgPuVdDo=',
+      title: 'Fake Event 6',
+      description: 'This is a fake event description 5',
+    },
+    {
+      id: 7,
+      imageSrc:
+        'https://media.istockphoto.com/id/1297159365/photo/portrait-of-young-smiling-woman-face-partially-covered-with-flying-hair-in-windy-day-standing.jpg?s=612x612&w=0&k=20&c=I16c_ZzQHEeGoPUVrVP9pPusSzsmymvVKdQVgPuVdDo=',
+      title: 'Fake Event 7',
+      description: 'This is a fake event description 5',
+    },
+  ]
