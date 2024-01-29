e94158e768f4d8e0c1b215935b913917a0e4e452
Omkar Date <omkardate29@gmail.com>
2024-01-29 14:32:27 +0530

    fix: nav fix

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -4,9 +4,9 @@ export default function Navbar() {
   return (
     <div
       id='navbar'
-      className='fixed bottom-4 left-[50%] z-20 h-[60px] min-w-fit -translate-x-[50%] rounded-full bg-white/30 p-8 backdrop-blur-md'
+      className='fixed bottom-4 left-[50%] z-20 h-[60px] min-w-fit -translate-x-[50%] rounded-full bg-white/20 p-8 backdrop-blur-md'
     >
-      <ul className='flex h-full flex-row items-center justify-around gap-8 font-bold text-stone-800'>
+      <ul className='flex h-full flex-row items-center justify-around gap-8 font-bold text-stone-50'>
         <Link href="#home">Home</Link>
         <Link href="#about">About</Link>
         <Link href="#sponsors" className='hidden md:block'>Sponsors</Link>
